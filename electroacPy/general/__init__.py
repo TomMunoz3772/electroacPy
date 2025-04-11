cde3bf0c0a06bd506914e7139d452bbb08741272
Tom M. <munozt@outlook.fr>
2025-04-11 09:44:35 +0200

    small box builder

--- electroacPy/general/__init__.py
+++ electroacPy/general/__init__.py
@@ -5,6 +5,7 @@ from . import geometry
 from . import io
 from . import signals
 from . import plot
+from .boxBuilder import shoebox
 from .mesh import CAD as meshCAD
 import numpy as np
 from re import findall
